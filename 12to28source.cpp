688e5fb8407b05986119c33872f4953eeda42483
Catherine Kimani <catherine.wachu@strathmore.edu>
2021-11-22 22:48:10 +0300

    Add project files.

---
+++ 12to28source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int num = 12, increment = 2;
+	while (num <= 28) {
+		cout << num << endl;
+		num = num + increment;
+
+	}
+
+	return 0;
+}
